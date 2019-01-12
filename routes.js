0be291876687337811067bc3007833ded93a7a09
Olga <oserenchenko@gmail.com>
2019-01-12 15:56:04 -0700

    file structure, server set up

---
+++ routes.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  res.render("index");
+});
+
+router.get("/:url", function(req, res) {
+
+});
+
+// Export routes for server.js to use.
+module.exports = router;
