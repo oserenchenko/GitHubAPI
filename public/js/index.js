73d9279f1f85900d8ab3518ce908b3dada4fcffb
Olga <oserenchenko@gmail.com>
2019-01-14 18:53:57 -0700

    handling errors

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,27 @@
+
+
+//ajax call to GitHub API using input repository URL
 function gitHubCall(repoURL) {
   let urlCall = "https://api.github.com/repos" + repoURL + "/pulls";
   console.log(urlCall);
   $.ajax({
     url: urlCall,
-    method: "GET"
-  }).then(function(response) {
+    method: "GET",
+    success: function(response) {
     console.log(response);
-  })
+    if (response[0]) {
+      $(".response").text("This repository does have active pull requests.")
+    } else {
+      $(".response").text("This repository does NOT have active pull requests.")
+    }
+  },
+    error: function(request,status,errorThrown) {
+      $(".response").text("There has been an error, please check your repository URL to make sure it is correct.");
+    }
+})
 }
 
+//Handling submit button click event - if the input has a value, use it to call the gitHubCall function
 $(".btn").on("click", function(event) {
   event.preventDefault();
   let repoURL = $("#inputRepoURL").val().trim().slice(18);
@@ -18,5 +31,6 @@ $(".btn").on("click", function(event) {
     gitHubCall(repoURL);
   } else {
     console.log("Please input a repository URL");
+    $(".response").text("Please input a repository URL.");
   }
 })
